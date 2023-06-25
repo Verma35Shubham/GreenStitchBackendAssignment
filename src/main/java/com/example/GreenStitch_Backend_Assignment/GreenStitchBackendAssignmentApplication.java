f14d0b56e9a6ebfb932b6ff5196e9ba4fab92a5f
unknown <verma.shubham2537@gmail.com>
2023-06-25 17:31:51 +0530

    24 Commit-2

--- src/main/java/com/example/GreenStitch_Backend_Assignment/GreenStitchBackendAssignmentApplication.java
+++ src/main/java/com/example/GreenStitch_Backend_Assignment/GreenStitchBackendAssignmentApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GreenStitchBackendAssignmentApplication {
-
-
 	public static void main(String[] args) {
 		SpringApplication.run(GreenStitchBackendAssignmentApplication.class, args);
 	}
